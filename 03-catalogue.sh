17da7fef3fb5dbbce66438fcfabefd04f059bec3
nischiashok <126067320+nischiashok@users.noreply.github.com>
2025-07-20 12:15:48 +0530

    Update 03-catalogue.sh

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -77,10 +77,10 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 
-STATUS=$(mongosh --host mongodb.srivenkata.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.jaiganesha.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.srivenkata.shop </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.jaiganesha.shop </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
